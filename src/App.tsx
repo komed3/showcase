514fa193479a660c22100769a33cdd26ca25e83b
komed3 <pk97@outlook.de>
2026-04-06 10:43:57 +0200

    use showcase logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { useShowcase } from '@/src/showcase';
+
 export default function App () {
+    const {
+        url, inputUrl, setInputUrl, zoom, setZoom, devices, showInfo, setShowInfo,
+        showAddModal, setShowAddModal, isDragging, loadError, scrollContainerRef,
+        handleUrlSubmit, handleAddDevice, removeDevice, toggleDeviceOrientation,
+        resetWorkspace, handleMouseDown, handleMouseMove, handleMouseUp
+    } = useShowcase();
+
     return ( <></> );
 }
